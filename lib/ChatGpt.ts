530a5d708d371c1ee2f8c2ba1d96bfb8b4de765d
Marius Brt <marius.brouty@gmail.com>
2023-12-13 00:01:56 +0100

    prompt

--- lib/ChatGpt.ts
+++ lib/ChatGpt.ts
@@ -1,31 +1,33 @@
-import OpenAI from 'openai';
-import {ChatCompletionMessageParam} from "openai/src/resources/chat/completions";
+import OpenAI from "openai";
+import { ChatCompletionMessageParam } from "openai/src/resources/chat/completions";
+import { readFileSync } from "fs";
 
 interface Message {
-    role: string;
-    content: string;
+  role: string;
+  content: string;
 }
 
-export async function SendRequest(fullHistory: Array<ChatCompletionMessageParam>, text: string) {
-    const openai = new OpenAI({
-        apiKey: process.env.CHATGPT
-    });
+export async function SendRequest(
+  fullHistory: Array<ChatCompletionMessageParam>,
+  text: string
+) {
+  const openai = new OpenAI({
+    apiKey: process.env.CHATGPT,
+  });
+  const prompt = readFileSync("./lib/prompt.txt", "utf-8");
+  if (fullHistory.length === 0)
+    fullHistory.push({ content: prompt, role: "system" });
+  fullHistory.push({ content: text, role: "user" });
 
-    const prompt =
-        "Tu es un chatbot sur un site qui parle d'écologie et de changement climatique. Tu dois répondre uniquement à des questions concernant le thème du site. Réponds 'Désolé, je ne peux pas répondre à cette question' si la question est hors sujet. Tu dois répondre de manière concise. Ne fais pas plus de 2 phrases.";
+  const response = await openai.chat.completions.create({
+    messages: fullHistory,
+    model: "gpt-3.5-turbo",
+  });
 
-    if (fullHistory.length === 0)
-        fullHistory.push({content: prompt, role: "system"})
-    fullHistory.push({content: text, role: "user"})
+  fullHistory.push({
+    role: "assistant",
+    content: response.choices[0].message.content,
+  });
 
-    const response = await openai.chat.completions.create({
-        messages: fullHistory,
-        model: 'gpt-3.5-turbo',
-    });
-
-
-    // Ajouter la réponse générée à l'historique
-    fullHistory.push({role: 'assistant', content: response.choices[0].message.content});
-
-    return fullHistory;
+  return fullHistory;
 }
